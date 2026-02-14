e64db6643c0515d012abfd10f10088cfc381cfb3
Ubuntu <ubuntu@ip-172-31-45-186.ap-southeast-2.compute.internal>
2026-02-14 10:24:24 +0000

    fix: use compat Firebase providers in app.config
    Services use AngularFireAuth and AngularFirestore (compat API),
    so app.config must use compat providers instead of modular API.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,49 +14,26 @@
  * limitations under the License.
  */
 
-import { ApplicationConfig, provideZoneChangeDetection, ErrorHandler } from '@angular/core';
-import { initializeApp, provideFirebaseApp, getApp } from '@angular/fire/app';
-import {
-  ReCaptchaEnterpriseProvider,
-  initializeAppCheck,
-  provideAppCheck,
-} from '@angular/fire/app-check';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
+import { ApplicationConfig, provideZoneChangeDetection, ErrorHandler, importProvidersFrom } from '@angular/core';
 import { environment } from '../environments/environment';
 import { ErrorService } from './services/error.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-declare global {
-  var FIREBASE_APPCHECK_DEBUG_TOKEN: string;
-}
+// Firebase compat imports (required for AngularFireAuth and AngularFirestore)
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: ErrorHandler, useClass: ErrorService },
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideFirebaseApp(() =>
-      initializeApp(environment.firebase), 
+    // Use compat modules for AngularFireAuth and AngularFirestore
+    importProvidersFrom(
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireAuthModule,
+      AngularFirestoreModule
     ),
-    // Turn on app check for Vertex AI in Firebase
-    // provideAppCheck(() => {
-      // TODO: don't use debug token in prod
-      // self.FIREBASE_APPCHECK_DEBUG_TOKEN = environment.debug_token;
-
-      // const appCheck = initializeAppCheck(getApp(), {
-      //   provider: new ReCaptchaEnterpriseProvider("your site key here"),
-      //   isTokenAutoRefreshEnabled: true,
-      // });
-      // return appCheck;
-    // }),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => 
-      initializeFirestore(getApp(), {
-        localCache: persistentLocalCache({
-          tabManager: persistentMultipleTabManager(),
-        }),
-      })
-    ),
-    provideAnimationsAsync(), 
+    provideAnimationsAsync(),
   ],
 };
